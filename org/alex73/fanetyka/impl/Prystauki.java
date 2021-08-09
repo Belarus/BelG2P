23dee270d305048dcdb343341d78618cb22e6704
Aleś Bułojčyk <alex73mail@gmail.com>
2021-08-09 12:22:36 +0300

    fanetyka

--- org/alex73/fanetyka/impl/Prystauki.java
+++ org/alex73/fanetyka/impl/Prystauki.java
@@ -1,22 +1,20 @@
 package org.alex73.fanetyka.impl;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.text.Collator;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.commons.io.FileUtils;
 
 public class Prystauki {
 
-    static Set<String> prystauki = new TreeSet<>();
-
     public static void main(String[] args) throws Exception {
-        List<String> lines = FileUtils.readLines(new File("/home/alex/gits/GrammarDB/slovautvarennie.2.csv"),
-                "UTF-8");
+        List<String> lines = FileUtils
+                .readLines(new File("src-fanetyka/org/alex73/fanetyka/impl/slovautvarennie.2.txt"), "UTF-8");
         for (String line : lines) {
             String[] ws = line.split("\t");
             if (ws.length != 3) {
@@ -24,34 +22,31 @@ public class Prystauki {
             }
             process(ws[2]);
         }
-        List<String> out = new ArrayList<>(prystauki);
-        Collections.sort(out, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                StringBuilder s1=new StringBuilder(o1.replace("-", "").replace("'", ""));
-                StringBuilder s2=new StringBuilder(o2.replace("-", "").replace("'", ""));
-                return s1.reverse().toString().compareTo(s2.reverse().toString());
-            }
-        });
 
-        out.forEach(w -> System.out.println(w));
-        System.out.println(prystauki.size());
+        prystauki.entrySet().stream().filter(p->p.getValue()>0).forEach(w -> System.out.println(w));
     }
 
     static void process(String w) {
-        w = w.replace("!", "").replace("[", "");
-        int pos = w.indexOf("//");
-        if (pos < 0) {
+        int p = w.indexOf("//");
+        if (p <= 0)
             return;
-        }
-        w = w.substring(0, pos).replaceAll("\\-+$", "");
-        if (w.isEmpty()) {
-            return;
-        }
-        if ("уўеыаоэёяію".indexOf(w.charAt(w.length() - 1)) >= 0) {
-            return;
-        }
-        prystauki.add(w);
+
+        String pry = w.substring(0, p);
+        Integer c = prystauki.get(pry);
+        prystauki.put(pry, c == null ? 1 : c.intValue() + 1);
     }
 
+    static Comparator<String> P = new Comparator<String>() {
+        Collator BE = Collator.getInstance(new Locale("be"));
+
+        @Override
+        public int compare(String o1, String o2) {
+            int c = BE.compare(o1.replaceAll("[\\-!']", ""), o2.replaceAll("[\\-!']", ""));
+            if (c == 0) {
+                c = BE.compare(o1, o2);
+            }
+            return c;
+        }
+    };
+    static Map<String, Integer> prystauki = new TreeMap<>(P);
 }
