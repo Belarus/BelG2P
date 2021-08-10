1a1a70b228594274928b3df82ef94d5e4d1cffba
Aleś Bułojčyk <alex73mail@gmail.com>
2021-08-10 09:22:04 +0300

    аднаўленне пачатковых слоў

--- org/alex73/fanetyka/impl/Prystauki.java
+++ org/alex73/fanetyka/impl/Prystauki.java
@@ -2,6 +2,7 @@ package org.alex73.fanetyka.impl;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.Collator;
 import java.util.Comparator;
 import java.util.List;
@@ -17,12 +18,52 @@ public class Prystauki {
     // static final String FN =
     // "src-fanetyka/org/alex73/fanetyka/impl/slovautvarennie.2.txt";
     static final String FN = "src-fanetyka/org/alex73/fanetyka/impl/slovautvarennie.2.txt";
+    static final String FN3 = "src-fanetyka/org/alex73/fanetyka/impl/slovautvarennie.3.txt";
     static final String MAR = "src-fanetyka/org/alex73/fanetyka/impl/marfiemy.txt";
+    static final String TEZA = "src-fanetyka/org/alex73/fanetyka/impl/slovautv_teza.txt";
     static final Pattern RE1 = Pattern.compile("(\\{[0-9]+[abа]?\\. ?\\} ?).+");
     static final Pattern RE2 = Pattern.compile("(\\[[0-9]+\\.\\] ).+");
     static final Pattern RE3 = Pattern.compile("([0-9]+a?\\. ).+");
 
     public static void main(String[] args) throws Exception {
+        teza();
+    }
+
+    static void teza() throws Exception {
+        Map<String, String> r = new TreeMap<>(P);
+        for (String line : Files.readAllLines(Paths.get(TEZA))) {
+            String[] ws = line.split("\t");
+            ws[0] = ws[0].replaceAll("[\\-!']", "");
+            String prev = r.put(ws[0], line);
+            if (prev != null) {
+                System.err.println("Duplicate: " + line + " /// " + prev);
+            }
+        }
+    }
+
+    static void k2() throws Exception {
+        List<String> lines = FileUtils.readLines(new File(FN3), "UTF-8");
+        for (String line : lines) {
+            String[] ws = line.split("\t");
+            if (ws[1].equals("???") && ws[2].replaceAll("\\([^\\)]+\\)", "").replaceAll("[^/]", "").length() >= 6) {
+                System.out.println(line);
+            }
+        }
+    }
+
+    static void uzn() throws Exception {
+        List<String> lines = FileUtils.readLines(new File(FN3), "UTF-8");
+        for (int i = 0; i < lines.size(); i++) {
+            String[] ws = lines.get(i).trim().split("\t");
+            if (ws[0].equals("???")) {
+                ws[0] = ws[2].replaceAll("\\([^\\)]+\\)", "").replace("-", "").replace("/", "");
+            }
+            lines.set(i, String.join("\t", ws));
+        }
+        FileUtils.writeLines(new File(FN3), lines);
+    }
+
+    static void merge() throws Exception {
         List<String> lines = FileUtils.readLines(new File(FN), "UTF-8");
         List<String> mar = FileUtils.readLines(new File(MAR), "UTF-8");
         nnn: for (int i = 0, m = 0; i < lines.size();) {
