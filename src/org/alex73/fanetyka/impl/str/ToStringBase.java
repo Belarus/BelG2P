2b9a51a495cb420fd37f768873f89a967f5a9b01
Aleś Bułojčyk <alex73mail@gmail.com>
2025-09-08 23:37:51 +0300

    Лепшы вывад націскаў

--- src/org/alex73/fanetyka/impl/str/ToStringBase.java
+++ src/org/alex73/fanetyka/impl/str/ToStringBase.java
@@ -1,5 +1,6 @@
 package org.alex73.fanetyka.impl.str;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.alex73.fanetyka.impl.Huk;
@@ -17,27 +18,52 @@ public abstract class ToStringBase {
     abstract protected char getPadvojenyChar();
 
     public String toString(List<Huk> huki) {
+        // папярэдняя канвертацыя
+        List<HukChars> hs = new ArrayList<>(huki.stream().map(h -> new HukChars(h)).toList());
+
+        // падваенне
+        for (int i = 1; i < hs.size(); i++) {
+            HukChars p = hs.get(i - 1);
+            HukChars c = hs.get(i);
+            if (p.zycny && c.zycny && p.str.equals(c.str)) {
+                p.str += getPadvojenyChar();
+                p.ipaStressBefore |= c.ipaStressBefore;
+                p.stressAfter |= c.stressAfter;
+                p.spaceAfter = c.spaceAfter;
+                hs.remove(i);
+                i--;
+            }
+        }
+
+        // вывад у адзін радок
+        hs.getLast().spaceAfter = false;
         StringBuilder out = new StringBuilder();
-        Huk prevHuk = null;
-        String prevHukStr = "";
-        for (Huk huk : huki) {
-            String hukStr = huk2str(huk);
-            if (prevHuk != null && prevHuk.stressIpa && getIpaStressChar() != 0) {
+        for (HukChars h : hs) {
+            if (h.ipaStressBefore && getIpaStressChar() != 0) {
                 out.append(getIpaStressChar());
             }
-            out.append(prevHukStr);
-            if (prevHuk != null && prevHuk.stress && getStressChar() != 0) {
+            out.append(h.str);
+            if (h.stressAfter && getStressChar() != 0) {
                 out.append(getStressChar());
             }
-            if (!Huk.halosnyja.contains(huk.bazavyHuk) && hukStr.equals(prevHukStr)) {
-                hukStr = Character.toString(getPadvojenyChar());
-            } else if (prevHuk != null && (prevHuk.padzielPasla & (Huk.PADZIEL_SLOVY | Huk.PADZIEL_PRYNAZOUNIK)) != 0) {
+            if (h.spaceAfter) {
                 out.append(' ');
             }
-            prevHuk = huk;
-            prevHukStr = hukStr;
         }
-        out.append(prevHukStr);
         return out.toString();
+    }
+
+    public class HukChars {
+        boolean ipaStressBefore, stressAfter, spaceAfter;
+        boolean zycny;
+        String str;
+
+        public HukChars(Huk huk) {
+            str = huk2str(huk);
+            ipaStressBefore = huk.stressIpa;
+            stressAfter = huk.stress;
+            spaceAfter = (huk.padzielPasla & (Huk.PADZIEL_SLOVY | Huk.PADZIEL_PRYNAZOUNIK)) != 0;
+            zycny = !Huk.halosnyja.contains(huk.bazavyHuk);
+        }
     }
 }
