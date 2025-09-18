37744c207bcbe8638c1f83d4f26743ebb59a7232
Aleś Bułojčyk <alex73mail@gmail.com>
2025-09-18 19:34:39 +0300

    Выпраўленні

--- src/org/alex73/fanetyka/impl/str/ToStringArfaep.java
+++ src/org/alex73/fanetyka/impl/str/ToStringArfaep.java
@@ -1,8 +1,5 @@
 package org.alex73.fanetyka.impl.str;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.alex73.fanetyka.impl.Huk;
 import org.alex73.fanetyka.impl.IPAUtils;
 import org.alex73.fanetyka.impl.IPAUtils.IPA;
@@ -11,23 +8,63 @@ import org.alex73.fanetyka.impl.IPAUtils.IPA;
  * Канвертавання гукаў у String для фармату арфаэпічнага слоўніка.
  */
 public class ToStringArfaep extends ToStringBase {
-    static final Map<IPA, String> ARFAEP_MAP = new HashMap<>();
-    static {
-        ARFAEP_MAP.putAll(ToStringSkolny.SKOLNY_MAP);
-        ARFAEP_MAP.put(IPA.ɣ, "γ");
-        ARFAEP_MAP.put(IPA.ɣʲ, "γ'");
-        ARFAEP_MAP.put(IPA.g, "g");
-        ARFAEP_MAP.put(IPA.gʲ, "g'");
-        ARFAEP_MAP.put(IPA.d͡ʐ, "ž");
-        ARFAEP_MAP.put(IPA.d͡z, "z");
-        ARFAEP_MAP.put(IPA.d͡zʲ, "z'");
-        ARFAEP_MAP.put(IPA.ɐ, "а");
-        ARFAEP_MAP.put(IPA.ə, "ы'");
-    }
-
     @Override
     protected String huk2str(Huk h) {
-        return ARFAEP_MAP.get(IPAUtils.huk2ipa(h));
+        IPA ipa = IPAUtils.huk2ipa(h);
+        return switch (ipa) {
+        case IPA.a -> "а";
+        case IPA.b -> "б";
+        case IPA.bʲ -> "б'";
+        case IPA.v -> "в";
+        case IPA.vʲ -> "в'";
+        case IPA.β -> "в";
+        case IPA.βʲ -> "в'";
+        case IPA.ɣ -> "γ";
+        case IPA.ɣʲ -> "γ'";
+        case IPA.g -> "g";
+        case IPA.gʲ -> "g'";
+        case IPA.d -> "д";
+        case IPA.dʲ -> "д'";
+        case IPA.ɛ -> "э";
+        case IPA.d͡ʐ -> "ž";
+        case IPA.d͡z -> "z";
+        case IPA.d͡zʲ -> "z'";
+        case IPA.ʐ -> "ж";
+        case IPA.z -> "з";
+        case IPA.zʲ -> "з'";
+        case IPA.i -> "і";
+        case IPA.k -> "к";
+        case IPA.kʲ -> "к'";
+        case IPA.ɫ -> "л";
+        case IPA.lʲ -> "л'";
+        case IPA.m -> "м";
+        case IPA.mʲ -> "м'";
+        case IPA.ɱ -> "м";
+        case IPA.n -> "н";
+        case IPA.nʲ -> "н'";
+        case IPA.ɔ -> "о";
+        case IPA.p -> "п";
+        case IPA.pʲ -> "п'";
+        case IPA.r -> "р";
+        case IPA.s -> "с";
+        case IPA.sʲ -> "с'";
+        case IPA.t -> "т";
+        case IPA.tʲ -> "т'";
+        case IPA.u -> "у";
+        case IPA.u̯ -> "ў";
+        case IPA.f -> "ф";
+        case IPA.fʲ -> "ф'";
+        case IPA.x -> "х";
+        case IPA.xʲ -> "х'";
+        case IPA.t͡s -> "ц";
+        case IPA.t͡sʲ -> "ц'";
+        case IPA.t͡ʂ -> "ч";
+        case IPA.ʂ -> "ш";
+        case IPA.ɨ -> "ы";
+        case IPA.j -> "й";
+        case IPA.ɐ -> "ӑ";
+        case IPA.ə -> "ъ";
+        };
     }
 
     @Override
--- src/org/alex73/fanetyka/impl/str/ToStringSkolny.java
+++ src/org/alex73/fanetyka/impl/str/ToStringSkolny.java
@@ -1,8 +1,5 @@
 package org.alex73.fanetyka.impl.str;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.alex73.fanetyka.impl.Huk;
 import org.alex73.fanetyka.impl.IPAUtils;
 import org.alex73.fanetyka.impl.IPAUtils.IPA;
@@ -13,65 +10,63 @@ import org.alex73.fanetyka.impl.IPAUtils.IPA;
  * Combining Double Inverted Breve - U+0361
  */
 public class ToStringSkolny extends ToStringBase {
-    static final Map<IPA, String> SKOLNY_MAP = new HashMap<>();
-    static {
-        SKOLNY_MAP.put(IPA.a, "а");
-        SKOLNY_MAP.put(IPA.b, "б");
-        SKOLNY_MAP.put(IPA.bʲ, "б'");
-        SKOLNY_MAP.put(IPA.v, "в");
-        SKOLNY_MAP.put(IPA.vʲ, "в'");
-        SKOLNY_MAP.put(IPA.β, "в");
-        SKOLNY_MAP.put(IPA.βʲ, "в'");
-        SKOLNY_MAP.put(IPA.ɣ, "г");
-        SKOLNY_MAP.put(IPA.ɣʲ, "г'");
-        SKOLNY_MAP.put(IPA.g, "ґ");
-        SKOLNY_MAP.put(IPA.gʲ, "ґ'");
-        SKOLNY_MAP.put(IPA.d, "д");
-        SKOLNY_MAP.put(IPA.dʲ, "д'");
-        SKOLNY_MAP.put(IPA.ɛ, "э");
-        SKOLNY_MAP.put(IPA.d͡ʐ, "д\u0361ж");
-        SKOLNY_MAP.put(IPA.d͡z, "д\u0361з");
-        SKOLNY_MAP.put(IPA.d͡zʲ, "д\u0361з'");
-        SKOLNY_MAP.put(IPA.ʐ, "ж");
-        SKOLNY_MAP.put(IPA.z, "з");
-        SKOLNY_MAP.put(IPA.zʲ, "з'");
-        SKOLNY_MAP.put(IPA.i, "і");
-        SKOLNY_MAP.put(IPA.k, "к");
-        SKOLNY_MAP.put(IPA.kʲ, "к'");
-        SKOLNY_MAP.put(IPA.ɫ, "л");
-        SKOLNY_MAP.put(IPA.lʲ, "л'");
-        SKOLNY_MAP.put(IPA.m, "м");
-        SKOLNY_MAP.put(IPA.mʲ, "м'");
-        SKOLNY_MAP.put(IPA.ɱ, "м");
-        SKOLNY_MAP.put(IPA.n, "н");
-        SKOLNY_MAP.put(IPA.nʲ, "н'");
-        SKOLNY_MAP.put(IPA.ɔ, "о");
-        SKOLNY_MAP.put(IPA.p, "п");
-        SKOLNY_MAP.put(IPA.pʲ, "п'");
-        SKOLNY_MAP.put(IPA.r, "р");
-        SKOLNY_MAP.put(IPA.s, "с");
-        SKOLNY_MAP.put(IPA.sʲ, "с'");
-        SKOLNY_MAP.put(IPA.t, "т");
-        SKOLNY_MAP.put(IPA.tʲ, "т'");
-        SKOLNY_MAP.put(IPA.u, "у");
-        SKOLNY_MAP.put(IPA.u̯, "ў");
-        SKOLNY_MAP.put(IPA.f, "ф");
-        SKOLNY_MAP.put(IPA.fʲ, "ф'");
-        SKOLNY_MAP.put(IPA.x, "х");
-        SKOLNY_MAP.put(IPA.xʲ, "х'");
-        SKOLNY_MAP.put(IPA.t͡s, "ц");
-        SKOLNY_MAP.put(IPA.t͡sʲ, "ц'");
-        SKOLNY_MAP.put(IPA.t͡ʂ, "ч");
-        SKOLNY_MAP.put(IPA.ʂ, "ш");
-        SKOLNY_MAP.put(IPA.ɨ, "ы");
-        SKOLNY_MAP.put(IPA.j, "й");
-        SKOLNY_MAP.put(IPA.ɐ, "ӑ");// "\u2009\u2DF6");// 2DF6
-        SKOLNY_MAP.put(IPA.ə, "ъ");// "\u2009\uA679"); // A679
-    }
-
     @Override
     public String huk2str(Huk h) {
-        return SKOLNY_MAP.get(IPAUtils.huk2ipa(h));
+        IPA ipa = IPAUtils.huk2ipa(h);
+        return switch (ipa) {
+        case IPA.a -> "а";
+        case IPA.b -> "б";
+        case IPA.bʲ -> "б'";
+        case IPA.v -> "в";
+        case IPA.vʲ -> "в'";
+        case IPA.β -> "в";
+        case IPA.βʲ -> "в'";
+        case IPA.ɣ -> "г";
+        case IPA.ɣʲ -> "г'";
+        case IPA.g -> "ґ";
+        case IPA.gʲ -> "ґ'";
+        case IPA.d -> "д";
+        case IPA.dʲ -> "д'";
+        case IPA.ɛ -> "э";
+        case IPA.d͡ʐ -> "д\u0361ж";
+        case IPA.d͡z -> "д\u0361з";
+        case IPA.d͡zʲ -> "д\u0361з'";
+        case IPA.ʐ -> "ж";
+        case IPA.z -> "з";
+        case IPA.zʲ -> "з'";
+        case IPA.i -> "і";
+        case IPA.k -> "к";
+        case IPA.kʲ -> "к'";
+        case IPA.ɫ -> "л";
+        case IPA.lʲ -> "л'";
+        case IPA.m -> "м";
+        case IPA.mʲ -> "м'";
+        case IPA.ɱ -> "м";
+        case IPA.n -> "н";
+        case IPA.nʲ -> "н'";
+        case IPA.ɔ -> "о";
+        case IPA.p -> "п";
+        case IPA.pʲ -> "п'";
+        case IPA.r -> "р";
+        case IPA.s -> "с";
+        case IPA.sʲ -> "с'";
+        case IPA.t -> "т";
+        case IPA.tʲ -> "т'";
+        case IPA.u -> "у";
+        case IPA.u̯ -> "ў";
+        case IPA.f -> "ф";
+        case IPA.fʲ -> "ф'";
+        case IPA.x -> "х";
+        case IPA.xʲ -> "х'";
+        case IPA.t͡s -> "ц";
+        case IPA.t͡sʲ -> "ц'";
+        case IPA.t͡ʂ -> "ч";
+        case IPA.ʂ -> "ш";
+        case IPA.ɨ -> "ы";
+        case IPA.j -> "й";
+        case IPA.ɐ -> "ӑ";// "\u2009\u2DF6";// 2DF6
+        case IPA.ə -> "ъ";// "\u2009\uA679"; // A679
+        };
     }
 
     @Override
